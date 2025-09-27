cc541c9654fc2dc23e952f8153e6c6632d476c67
K1eranza <zubrevaevelina20641@gmail.com>
2025-09-27 14:33:15 +0300

    шаг6.2

--- src/DungeonEscape.kt
+++ src/DungeonEscape.kt
@@ -56,10 +56,18 @@ fun main(){
 //    val sentence = "Kotlin is awesome"
 //    val words =sentence.split("")
 //    println(words)
-    val data = "apple, banana, orange"
-    val fruits = data.split(",")
-    for (fruit in fruits)
-    {
-        println(fruit)
+//    val data = "apple, banana, orange"
+//    val fruits = data.split(",")
+//    for (fruit in fruits)
+//    {
+//        println(fruit)
+//    }
+    print("Введите числа через пробел: ")
+    val input = readln()
+    val numbers = input.split(" ")
+    var sum = 0
+    for (number in numbers) {
+        sum += number.toInt()
     }
+    println("Сумма чисел: $sum")
 }
