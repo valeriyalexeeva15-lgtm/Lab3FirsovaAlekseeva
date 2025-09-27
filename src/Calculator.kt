fa33150e3026a540028c4b68073bd19413fc8528
K1eranza <zubrevaevelina20641@gmail.com>
2025-09-27 14:47:11 +0300

    шаг8

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,16 +1,37 @@
+import kotlin.random.Random
 fun main(){
-    print("Введите первое число, знак операции и второе число через пробел: ")
-    val input = readln().split(" ")
-    val symbol = input[1]
-    val firstNumber = input[0].toDouble()
-    val secondNumber = input[2].toDouble()
-    var result = 0.0
-    when (symbol) {
-        "/"->result=firstNumber/secondNumber
-        "*" ->result=firstNumber*secondNumber
-        "+"->result=firstNumber+secondNumber
-        "-"->result=firstNumber-secondNumber
-        else->println("Некорректный ввод")
-    }
-    println("$firstNumber $symbol $secondNumber = $result")
+//    print("Введите первое число, знак операции и второе число через пробел: ")
+//    val input = readln().split(" ")
+//    val symbol = input[1]
+//    val firstNumber = input[0].toDouble()
+//    val secondNumber = input[2].toDouble()
+//    var result = 0.0
+//    when (symbol) {
+//        "/"->result=firstNumber/secondNumber
+//        "*" ->result=firstNumber*secondNumber
+//        "+"->result=firstNumber+secondNumber
+//        "-"->result=firstNumber-secondNumber
+//        else->println("Некорректный ввод")
+//    }
+//    println("$firstNumber $symbol $secondNumber = $result")
+//    val target = Random.nextInt(1,100)
+//    println("Угадай число от 1 до 100")
+//    while (true) {
+//        print("Введи число: ")
+//        val input = readln().toInt()
+//        when{
+//            input < target -> println("Загаданное число больше.")
+//            input > target -> println("Загаданное число меньше.")
+//            else -> {
+//                println("Поздравляю! Ты угадал число: $target")
+//                break
+//            }
+//        }
+//    }
+    val dice1=Random.nextInt(from = 1, until = 7)
+    val dice2=Random.nextInt(from = 1, until = 7)
+    val sum = dice1+dice2
+    println("Первый кубик: $dice1")
+    println("Второй кубик: $dice2")
+    println("Сумма: $sum")
 }
