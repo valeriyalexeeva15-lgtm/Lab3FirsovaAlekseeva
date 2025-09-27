17e1dc319f83e6daeeb82bfc3d9fff854e23b3cf
K1eranza <zubrevaevelina20641@gmail.com>
2025-09-27 14:06:16 +0300

    шаг2.3

--- src/Loop2.kt
+++ src/Loop2.kt
@@ -4,14 +4,22 @@ fun main(){
 //        println("Число: $number")
 //        number++
 // }
-    var number = 0
-   while (number < 10) {
-       number++
-       if (number == 3) {
-           println("Останавливаемся на $number")
-            continue
+//    var number = 0
+//   while (number < 10) {
+//       number++
+//       if (number == 3) {
+//           println("Останавливаемся на $number")
+//            continue
+//        }
+//       println(number)
+//    }
+    println("Напишите что-нибудь (для выхода введите 'выход'):")
+    while (true) {
+        val input= readLine()
+        if (input == "выход") {
+            println("До свидания!")
+            break
         }
-       println(number)
+        println("Вы ввели: $input")
     }
-
 }
