33a9395f2f5507f938e4b20349917e08ac36344c
K1eranza <zubrevaevelina20641@gmail.com>
2025-09-27 14:00:46 +0300

    шаг2

--- src/Loop2.kt
+++ src/Loop2.kt
@@ -1,7 +1,17 @@
 fun main(){
-    var number=1
-    while (number<=5){
-        println("Число: $number")
-        number++
-    }
+//    var number=1
+//    while (number<=5){
+//        println("Число: $number")
+//        number++
+// }
+//    var number = 1
+//    while (number <= 10) {
+//        println(number)
+//        number++
+//        if (number == 5) {
+//            println("Останавливаемся на $number")
+//            break
+//        }
+//    }
+
 }
